f6e9cec799ec949b0f306782eb93e4b68e32da10
Edi Carlos note <edi_csilva@hotmail.com>
2017-03-04 16:19:56 -0300

    finalização

--- app/src/main/java/br/com/k19/android/cap05_02/NotesDao.java
+++ app/src/main/java/br/com/k19/android/cap05_02/NotesDao.java
@@ -2,8 +2,12 @@ package br.com.k19.android.cap05_02;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Edi Carlos on 04/03/2017.
@@ -32,8 +36,42 @@ public class NotesDao {
 
         ContentValues values = new ContentValues();
         values.put(CustomSQLiteOpenHelper.COLUMN_NOTES, note);
-        
+        long insertId = database.insert(CustomSQLiteOpenHelper.TABLE_NOTES, null, values);
+        Cursor cursor = database.query(CustomSQLiteOpenHelper.TABLE_NOTES,
+                    columns, CustomSQLiteOpenHelper.COLUMN_ID + " = " + insertId, null,
+                null, null, null);
+        cursor.moveToFirst();
+        Note newNote = new Note();
+        newNote.setId(cursor.getLong(0));
+        newNote.setNote(cursor.getString(1));
+        cursor.close();
+        return newNote;
+    }
+
+    public void delete(Note note) {
+        long id = note.getId();
+        database.delete(CustomSQLiteOpenHelper.TABLE_NOTES, CustomSQLiteOpenHelper.COLUMN_ID
+                + " = " + id, null);
+    }
+
+    public List<Note> getAll() {
+        List<Note> notes = new ArrayList<Note>();
+
+        Cursor cursor = database.query(CustomSQLiteOpenHelper.TABLE_NOTES,
+                columns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Note note = new Note();
+            note.setId(cursor.getLong(0));
+            note.setNote(cursor.getString(1));
+            notes.add(note);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return notes;
 
     }
+
 
 }
